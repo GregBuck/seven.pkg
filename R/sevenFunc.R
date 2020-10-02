5f5334036ff56905730ee374831f1e98471e8bea
GregBuck <gregory.buck@alaska.gov>
2020-10-02 09:55:38 -0800

    Add files via upload

---
+++ R/sevenFunc.R
@@ -0,0 +1,45 @@
+
+#' multiply
+#'
+#' multiply numeric input by seven
+#' @param A numeric input 'x'
+#' @keywords seven
+#' @export
+#' @examples
+#' multiply()
+
+multiply <- function(x) {
+  y <- 7 * x
+  print("and the answer is...")
+  print(y)
+}
+
+#' divide
+#'
+#' divide numeric input by seven
+#' @param A numeric input 'x'
+#' @keywords seven
+#' @export
+#' @examples
+#' divide()
+
+divide <- function(x){
+  y <- x/7
+  print("and the answer is...")
+  print(y)
+}
+
+#' add
+#'
+#' add numeric input by seven
+#' @param A numeric input 'x'
+#' @keywords seven
+#' @export
+#' @examples
+#' add()
+
+add <- function(x){
+  y <- x + 7
+  print("and the answer is...")
+  print(y)
+}
